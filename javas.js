3aaa8a7313c18d664e4669860b192a4a5a902eca
muralirv <145271794+muralirv@users.noreply.github.com>
2024-04-10 11:28:44 +0530

    Add files via upload

---
+++ javas.js
@@ -0,0 +1,8 @@
+function fopen(){
+    const slide=document.querySelector('.side');
+    slide.style.display='flex'
+}
+function fclose(){
+    const slide=document.querySelector('.side');
+    slide.style.display='none'
+}
